0adcd2b11e6357e2877a2a3ac3d253d5e71ba32b
hanslhansl <>
2025-01-07 22:46:16 +0100

    Add project files.

---
+++ diniso1328/__init__.py
@@ -0,0 +1,5 @@
+"""Implementation of DIN ISO 1328 - Cylindrical gears – ISO system of flank tolerance classification.
+
+See https://github.com/hanslhansl/din3962."""
+
+from . import diniso1328_1
---
+++ diniso1328/diniso1328_1.py
@@ -0,0 +1,14 @@
+from enum import IntEnum
+
+class FlankToleranceClass(IntEnum):
+    ISO1 = 1
+    ISO2 = 2
+    ISO3 = 3
+    ISO4 = 4
+    ISO5 = 5
+    ISO6 = 6
+    ISO7 = 7
+    ISO8 = 8
+    ISO9 = 9
+    ISO10 = 10
+    ISO11 = 11
